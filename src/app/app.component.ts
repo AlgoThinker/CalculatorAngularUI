f05ac34b88ca90945934aa5b04a468972580ca15
Tushar Sharma <tushar123cse@gmail.com>
2019-11-12 01:41:45 +0530

    optimizing the code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,47 +11,62 @@ import { catchError, map, tap } from 'rxjs/operators';
 })
 export class AppComponent {
 
-
   constructor(private http: HttpClient) { }
+
+  private url = 'http://localhost:9200/api-gateway/';  // URL to web api
 
   public show: boolean = false;
   public expr: string = '';
   public result: any = '';
   public resultExp: any = '';
-  private url = 'http://localhost:9200/api-gateway/';  // URL to web api
 
-  getResult(expression: string) {
-    this.http.post<string>(this.url, expression).subscribe((val) => {
-      this.result = val;
-    },
-      response => {
-        console.log("POST call in error", response);
-      },
-      () => {
-        console.log("The POST observable is now completed.");
-      });
-  }
-
-  getResultExp(expression: string) {
-    this.http.post<string>(this.url, expression).subscribe((val) => {
-      this.resultExp = val;
-    },
-      response => {
-        console.log("POST call in error", response);
-      },
-      () => {
-        console.log("The POST observable is now completed.");
-      });
-  }
-
+ // public equalPressed: boolean = false;
 
   toggle() {
     this.show = !this.show;
   }
 
-  plusClick(expression: string) {
+  //method for UI Mode 2 with full expression
+  getResultExp(expression: string) {
+    this.http.post<string>(this.url, expression).subscribe((val) => {
+      this.resultExp = val;
+    },
+      response => {
+        this.resultExp = "Invalid Input";
+        console.log("POST call in error", response);
+      },
+      () => {
+        console.log("The POST observable is now completed.");
+      });
+  }
 
-    //  if(expression && expression != '' && expression == [0-9]* ){}
+  pressEqual(expression: string) {
+  //  this.equalPressed = true;
+    this.http.post<string>(this.url, expression).subscribe((val) => {
+      this.result = val;
+    },
+      response => {
+        this.result = "Invalid Input";
+        console.log("POST call in error", response);
+      },
+      () => {
+        console.log("The POST observable is now completed.");
+      });
+  }
 
+  pressPlus(expression: string) {
+
+  //   let term = "sample1";
+  //   let re = new RegExp("^([a-z0-9]{5,})$");
+  //   if (re.test(term)) {
+  //       console.log("Valid");
+  //   } else {
+  //       console.log("Invalid");
+  //   }
+  //   if(expression)
+
+  //   //  if(expression && expression != '' && expression == [0-9]* ){}
+
+  // }
   }
 }
