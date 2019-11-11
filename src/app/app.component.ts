994aebe30b3dccbd89150dc93bf8e717c02285e7
Tushar Sharma <tushar123cse@gmail.com>
2019-11-12 00:15:01 +0530

    optimizing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
+import { Observable, of } from 'rxjs';
+import { catchError, map, tap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
@@ -7,18 +11,47 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
 
+
+  constructor(private http: HttpClient) { }
+
   public show: boolean = false;
-  public expr:string = '';
-  
-  
+  public expr: string = '';
+  public result: any = '';
+  public resultExp: any = '';
+  private url = 'http://localhost:9200/api-gateway/';  // URL to web api
+
+  getResult(expression: string) {
+    this.http.post<string>(this.url, expression).subscribe((val) => {
+      this.result = val;
+    },
+      response => {
+        console.log("POST call in error", response);
+      },
+      () => {
+        console.log("The POST observable is now completed.");
+      });
+  }
+
+  getResultExp(expression: string) {
+    this.http.post<string>(this.url, expression).subscribe((val) => {
+      this.resultExp = val;
+    },
+      response => {
+        console.log("POST call in error", response);
+      },
+      () => {
+        console.log("The POST observable is now completed.");
+      });
+  }
+
 
   toggle() {
-   this.show = !this.show;
- }
+    this.show = !this.show;
+  }
 
- plusClick(expression:string){
+  plusClick(expression: string) {
 
- //  if(expression && expression != '' && expression == [0-9]* ){}
-    
- }
+    //  if(expression && expression != '' && expression == [0-9]* ){}
+
+  }
 }
